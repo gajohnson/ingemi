9f8defbdaca84a6183f57c0269e08232ef3f63fb
Geoffrey Johnson <gaj524@gmail.com>
2012-08-15 20:12:46 -0400

    Tweak compile script

--- compile.sh
+++ compile.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+
+cp js/ingemi.js demo/ingemi.js
+
 echo "Generating documentation ..."
+
 cd _jsdoc-toolkit
-java -jar jsrun.jar app/run.js -a -t=templates/jsdoc ../js/ingemi.js
-rm -rf ../docs
-mkdir ../docs
-mv ./out/jsdoc/* ../docs/
+
+java -jar jsrun.jar app/run.js -a -t=templates/jsdoc ../js/ingemi.js && \
+rm -rf ../docs && \
+mkdir ../docs && \
+mv ./out/jsdoc/* ../docs/ && \
 rm -rf ./out
+
 cd ..
+
 echo "Compiling javascript ..."
-cp js/ingemi.js closure-compiler/ingemi.js
+
 cd closure-compiler
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ingemi.js --js_output_file ingemi.min.js
-mv ingemi.min.js ingemi.js ../demo/
+
+cp ../js/ingemi.js ./ingemi.js && \
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ingemi.js --js_output_file ingemi.min.js && \
+rm ingemi.js
+mv ingemi.min.js ../demo/
+
 echo "Ingemi compiled successfully"
