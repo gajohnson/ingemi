891b4031afe738da8983c612505848de1ec323aa
Geoffrey Johnson <gaj524@gmail.com>
2012-08-29 18:00:21 -0400

    Add old files

---
+++ compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cp js/ingemi.js demo/ingemi.js
+
+echo "Generating documentation ..."
+
+cd _jsdoc-toolkit
+
+java -jar jsrun.jar app/run.js -a -t=templates/jsdoc ../js/ingemi.js && \
+rm -rf ../docs && \
+mkdir ../docs && \
+mv ./out/jsdoc/* ../docs/ && \
+rm -rf ./out
+
+cd ..
+
+echo "Compiling javascript ..."
+
+cd closure-compiler
+
+cp ../js/ingemi.js ./ingemi.js && \
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ingemi.js --js_output_file ingemi.min.js && \
+rm ingemi.js
+mv ingemi.min.js ../demo/
+
+echo "Ingemi compiled successfully"
