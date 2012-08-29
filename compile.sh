995470e6866eb4a9b5a632cd376f0eaba456a184
Geoffrey Johnson <gaj524@gmail.com>
2012-08-29 18:08:56 -0400

    Update build script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-cp js/ingemi.js ~/Sites/ingemi/ingemi.js
+echo "Compiling javascript ..."
+
+cd closure-compiler
+
+cp ../js/ingemi.js ./ingemi.js && \
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ingemi.js --js_output_file ingemi.min.js && \
+rm ingemi.js
+mv ingemi.min.js ~/Sites/ingemi/ingemi.js
+
+cp ../js/w.mandelbrot.js ./w.mandelbrot.js && \
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js w.mandelbrot.js --js_output_file w.mandelbrot.min.js && \
+rm w.mandelbrot.js
+mv w.mandelbrot.min.js ~/Sites/ingemi/w.mandelbrot.js
+
+echo "Ingemi compiled successfully"
+
+cd ..
+
+echo "Staging remaning files"
+
+cp index.html ~/Sites/ingemi/
 
 echo "Generating documentation ..."
 
@@ -13,19 +33,3 @@ mv ./out/jsdoc/* ../docs/ && \
 rm -rf ./out
 
 cd ..
-
-echo "Compiling javascript ..."
-
-cd closure-compiler
-
-cp ../js/ingemi.js ./ingemi.js && \
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ingemi.js --js_output_file ingemi.min.js && \
-rm ingemi.js
-mv ingemi.min.js ~/Sites/ingemi/
-
-cp ../js/w.mandelbrot.js ./w.mandelbrot.js && \
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js w.mandelbrot.js --js_output_file w.mandelbrot.js && \
-rm w.mandelbrot.js
-mv w.mandelbrot.js ~/Sites/ingemi/
-
-echo "Ingemi compiled successfully"
