57a94e0dd0935ff5db9e1dd41c668cfaa957aea3
Geoffrey Johnson <gaj524@gmail.com>
2012-08-29 17:58:50 -0400

    Restructure

--- webworkers/thread.js
+++
@@ -1,6 +0,0 @@
-self.onmessage = function(event) {
-  var buffer = event.data.buffer;
-  var fa = new Float32Array(buffer);
-  fa[event.data.idx]++;
-  self.webkitPostMessage({response: buffer, idx: event.data.idx}, [buffer]);
-};
