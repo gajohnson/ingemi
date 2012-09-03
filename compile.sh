0053d4c17e9f4450f9e94aaf2b2c09ca13e30b5c
Geoffrey Johnson <gaj524@gmail.com>
2012-09-03 18:38:14 -0400

    Streamline directory structure

--- compile.sh
+++ compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+OUT_DIR=~/Sites/ingemi
+
 if [ "$1" == debug ]; then
-cp ./js/ingemi.js ~/Sites/ingemi/
-cp ./js/w.mandelbrot.js ~/Sites/ingemi/
+cp ./js/ingemi.js $OUT_DIR/
+cp ./js/w.mandelbrot.js $OUT_DIR/
 else
 echo "Compiling javascript ..."
 
@@ -11,21 +13,16 @@ cd closure-compiler
 cp ../js/ingemi.js ./ingemi.js && \
 java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ingemi.js --js_output_file ingemi.min.js && \
 rm ingemi.js
-mv ingemi.min.js ~/Sites/ingemi/ingemi.js
+mv ingemi.min.js $OUT_DIR/ingemi.js
 
 cp ../js/w.mandelbrot.js ./w.mandelbrot.js && \
 java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js w.mandelbrot.js --js_output_file w.mandelbrot.min.js && \
 rm w.mandelbrot.js
-mv w.mandelbrot.min.js ~/Sites/ingemi/w.mandelbrot.js
+mv w.mandelbrot.min.js $OUT_DIR/w.mandelbrot.js
 
 echo "Ingemi compiled successfully"
 cd ..
 fi
-
-echo "Staging remaning files"
-
-cp index.html ~/Sites/ingemi/
-cp -r css ~/Sites/ingemi/
 
 echo "Generating documentation ..."
 
@@ -33,10 +30,10 @@ cd _jsdoc-toolkit
 
 java -jar jsrun.jar app/run.js -a -t=templates/jsdoc ../js/ingemi.js && \
 rm -rf ../docs && \
-mkdir ../docs && \
-mv ./out/jsdoc/* ../docs/ && \
+mkdir ../build/docs && \
+mv ./out/jsdoc/* ../build/docs/ && \
 rm -rf ./out
 
 cd ..
 
-cp -r ~/Sites/ingemi/* ./demo/
+cp -r $OUT_DIR/* ./build/
